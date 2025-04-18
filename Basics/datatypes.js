0dc49937eaf7d3b18fc8c4b6a33bf7c5d9dc521b
Shahzad Jatoi <shahzadjatoi1800@gmail.com>
2025-04-18 10:46:43 +0000

    Basics

---
+++ Basics/datatypes.js
@@ -0,0 +1,23 @@
+"use Strict" // treat all JS code as newer version 
+
+// window.alert("Shahzad");
+// alert.("Jatoi");
+// alert(3+3)
+// alert statement will print the result in the developer console of browser
+// alert have different syntax in node.js
+
+// int number --> 2 to pwr 56
+// bigint
+// String --> "s"
+// boolean
+// null --> it is actullay a stand alone value
+// undefined => we didn't assign value yet
+// symbol => to find the uniqueness
+
+// we have objects as well
+// typeof is used to find the type of any variable
+
+console.log(typeof "Shahzad");
+console.log(typeof null); // it give us object as an output
+console.log(typeof undefined); // while undefined is the type itself
+
---
+++ Basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 5143253
+let accountEmail = "shahzad@gmail.com"
+var accountPass = "12456"
+accountcity = "dadu" // not a good practice in js but still we can use it
+let accountstate // if we don't assign any value to this js will take it as undefined 
+
+/*
+prefer not to use var bcz issue in block scope and functional scope
+ what is the difference between let and var
+ we don't use var in js bcz it is causing the problem in scope like if someone create the same variable with same name 
+ and one is  changed other will automatellicaly change that is why let is used 
+*/
+
+console.log(accountcity);
+console.table([accountId, accountEmail,accountPass,accountcity,accountstate]);
+// semicolon is optional in js
